c63d08add51e337b42f52fcf917328ca2c924c9b
Túlio <tulioalvescor@gmail.com>
2023-07-16 14:10:37 -0300

    Correção Classe Animal e Impressão

--- SymbolicCreaturesSimulation.cpp
+++ SymbolicCreaturesSimulation.cpp
@@ -1,19 +1,109 @@
+//Codigo para testes
 #include <iostream>
 #include <vector>
 #include <tuple>
 #include <cstdlib>
 #include <ctime>
+#include <list>
 
 using namespace std;
 
 // Cores para representar os elementos
 #define RESET "\033[0m"     // Resetar para a cor padrão
-#define RED "\033[31m"      // Vermelho
-#define GREEN "\033[32m"    // Verde
-#define YELLOW "\033[33m"   // Amarelo
-#define BLUE "\033[34m"     // Azul
-#define MAGENTA "\033[35m"  // Magenta
-#define CYAN "\033[36m"     // Ciano
+#define CYAN "\033[36m"     // Ciano -> Arvores
+#define YELLOW "\033[33m"   // Amarelo -> Macacos
+#define BLUE "\033[34m"     // Azul -> Aguia
+#define MAGENTA "\033[35m"  // Magenta -> Tigre
+#define GREEN "\033[32m"    // Verde -> Cobra
+#define RED "\033[31m"      // Vermelho -> Morte
+
+// Classe abstrata Animal
+class Animal {
+public:
+    tuple<int, int> position; // Tupla de inteiros para a posição
+
+    virtual void perception() = 0; // Função abstrata para percepção
+    virtual void randomWalk() = 0; // Função abstrata para caminhada aleatória
+
+    virtual ~Animal() {}  // Destrutor virtual
+};
+
+// Classe Monkey (herda de Animal)
+class Monkey : public Animal {
+private:
+    string name; // Nome do Monkey
+
+public:
+    // Construtor
+    Monkey() {
+        
+    }
+
+    // Destrutor
+    ~Monkey() override {}
+
+    // Getter para o nome
+    string getName() const {
+        return name;
+    }
+
+    // Setter para o nome
+    void setName(const string& n) {
+        name = n;
+    }
+
+    void perception() override {
+        // Implemente o código da percepção do Monkey
+        cout << "Monkey " << name << " perceives its surroundings." << endl;
+    }
+
+    void randomWalk() override {
+        // Implemente o código da caminhada aleatória do Monkey
+        cout << "Monkey " << name << " is walking randomly." << endl;
+    }
+};
+
+// Classe Predator (herda de Animal)
+class Predator : public Animal {
+public:
+    enum predatorType {
+        TI, // Tiger
+        EA, // Eagle
+        SN  // Snake
+    };
+
+    int strength; // Força do Predator]
+    predatorType type;
+
+    // Construtor
+
+    Predator(predatorType t) {
+        this->type = t;
+    }
+
+    // Destrutor
+    ~Predator() override {}
+
+    // Getter para a força
+    int getStrength() const {
+        return strength;
+    }
+
+    // Setter para a força
+    void setStrength(int s) {
+        strength = s;
+    }
+
+    void perception() override {
+        // Implemente o código da percepção do Predator
+        cout << "Predator with strength " << strength << " perceives its surroundings." << endl;
+    }
+
+    void randomWalk() override {
+        // Implemente o código da caminhada aleatória do Predator
+        cout << "Predator with strength " << strength << " is walking randomly." << endl;
+    }
+};
 
 // Classe do Ambiente
 class Environment {
@@ -29,9 +119,8 @@ public:
     // Atributos do ambiente
     EnvironmentType type;
     tuple<int, int> position;
-    //Monkey* monkey = new Monkey();
-    //Predator predator;
-
+    list<Monkey> monkeyList;
+    list<Predator> predatorList;
 
     // Construtor da classe Environment
     Environment(){
@@ -83,8 +172,26 @@ public:
         for(int i=0; i<l; i++){
             for(int j=0; j<c; j++){
                 if(environment[i][j].type == GR){
-                    cout << GREEN << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
-                    << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    if(!environment[i][j].monkeyList.empty()){
+                        cout << YELLOW << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
+                        << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    }
+                    else if(environment[i][j].predatorList.front().type == Predator::SN){
+                        cout << GREEN << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
+                        << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    }
+                    else if(environment[i][j].predatorList.front().type == Predator::TI){
+                        cout << MAGENTA << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
+                        << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    }
+                    else if(environment[i][j].predatorList.front().type == Predator::EA){
+                        cout << BLUE << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
+                        << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    }
+                    else{
+                        cout << RESET << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
+                        << get<0>(environment[i][j].position) << "," << get<1>(environment[i][j].position) << ")| " << RESET;
+                    }
                 }
                 else if(environment[i][j].type == HT ){
                     cout << CYAN << "|" << Environment::environmentTypeToString(environment[i][j].type) << " ("
@@ -103,90 +210,39 @@ public:
         } 
     }
 
-    /*static void processMonkey(Monkey* monkey) {
-        // Processar o objeto Monkey
-        // ...
-    }*/
-};
-
-// Classe abstrata Animal
-class Animal {
-public:
-    tuple<int, int> position; // Tupla de inteiros para a posição
-
-    virtual void perception() = 0; // Função abstrata para percepção
-    virtual void randomWalk() = 0; // Função abstrata para caminhada aleatória
-
-    virtual ~Animal() {}  // Destrutor virtual
-};
-
-// Classe Monkey (herda de Animal)
-class Monkey : public Animal {
-private:
-    string name; // Nome do Monkey
-
-public:
-    // Construtor
-    Monkey() {
-        
+    // Função para adicionar macacos no ambiente
+    static void addMonkey(Environment environment[10][10], int l, int c, Monkey m) {
+        srand(time(nullptr)); // Configuração da semente do gerador de números aleatórios
+        int it = 1;
+        while(it != 0){
+            int i = rand() % l, j = rand() % c;
+            if(environment[i][j].type == GR && environment[i][j].monkeyList.empty()){
+                cout << "Monkey added in position " << i << ',' << j << ".\n";
+                environment[i][j].monkeyList.push_back(m);
+                it--;
+            }
+        }
     }
 
-    // Destrutor
-    ~Monkey() override {}
-
-    // Getter para o nome
-    string getName() const {
-        return name;
-    }
-
-    // Setter para o nome
-    void setName(const string& n) {
-        name = n;
-    }
-
-    void perception() override {
-        // Implemente o código da percepção do Monkey
-        cout << "Monkey " << name << " perceives its surroundings." << endl;
-    }
-
-    void randomWalk() override {
-        // Implemente o código da caminhada aleatória do Monkey
-        cout << "Monkey " << name << " is walking randomly." << endl;
-    }
-};
-
-// Classe Predator (herda de Animal)
-class Predator : public Animal {
-private:
-    int strength; // Força do Predator
-
-public:
-    // Construtor
-    Predator() {
-        
-    }
-
-    // Destrutor
-    ~Predator() override {}
-
-    // Getter para a força
-    int getStrength() const {
-        return strength;
-    }
-
-    // Setter para a força
-    void setStrength(int s) {
-        strength = s;
-    }
-
-    void perception() override {
-        // Implemente o código da percepção do Predator
-        cout << "Predator with strength " << strength << " perceives its surroundings." << endl;
-    }
-
-    void randomWalk() override {
-        // Implemente o código da caminhada aleatória do Predator
-        cout << "Predator with strength " << strength << " is walking randomly." << endl;
+    static void addPredator(Environment environment[10][10], int l, int c, Predator p) {
+        srand(time(nullptr)); // Configuração da semente do gerador de números aleatórios
+        int it = 1;
+        while(it != 0){
+            int i = rand() % l, j = rand() % c;
+            if(environment[i][j].type == GR && environment[i][j].monkeyList.empty() && environment[i][j].predatorList.empty()){
+                if(p.type == Predator::EA){
+                    cout << "Eagle added in position " << i << ',' << j << ".\n";
+                }
+                else if(p.type == Predator::TI){
+                    cout << "Tiger added in position " << i << ',' << j << ".\n";
+                }
+                else if(p.type == Predator::SN){
+                    cout << "Snake added in position " << i << ',' << j << ".\n";
+                }
+                environment[i][j].predatorList.push_back(p);
+                it--;
+            }
+        }
     }
 };
 
@@ -203,25 +259,22 @@ int main() {
     Environment::addTree(environment, l, c, Environment::MT, 4);
     Environment::addTree(environment, l, c, Environment::LT, 4);
 
+    //Adicionando macacos
+    Monkey monkey01, monkey02, monkey03, monkey04; // Alocação estatica do objeto Monkey
+    Environment::addMonkey(environment, l, c, monkey01);
+    Environment::addMonkey(environment, l, c, monkey02);
+    Environment::addMonkey(environment, l, c, monkey03);
+    Environment::addMonkey(environment, l, c, monkey04);
+
+    //Adicionando predadores
+    Predator snake(Predator::SN), eagle(Predator::EA), tiger(Predator::TI);
+    Environment::addPredator(environment, l, c, snake);
+    Environment::addPredator(environment, l, c, eagle);
+    Environment::addPredator(environment, l, c, tiger);
+    cout << endl;
+    
     // Imprimindo os tipos dos elementos da matriz
     Environment::printEnvironment(environment, l, c);
-    
-    Monkey* monkey = new Monkey(); // Alocação dinâmica do objeto Monkey
-    //Environment::processMonkey(monkey); // Chamada da função estática passando o ponteiro do objeto
-    delete monkey; // Liberar memória alocada para o objeto Monkey
-
-    /*Predator* predator = new Predator();
-
-    monkey->perception();
-    monkey->randomWalk();
-    cout << "Monkey name: " << monkey->getName() << endl;
-
-    predator->perception();
-    predator->randomWalk();
-    cout << "Predator strength: " << predator->getStrength() << endl;
-   
-    delete[] predator;*/
-    //delete[] monkey;
 
     return 0;
 }
